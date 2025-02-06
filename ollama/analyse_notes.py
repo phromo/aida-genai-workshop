5f0135081016c6e285c2a31b504f63c42c2e2c3e
Erik Ylipää <erik.ylipaa@gmail.com>
2025-02-06 08:47:22 +0100

    Fixed some issues

--- ollama/analyse_notes.py
+++ ollama/analyse_notes.py
@@ -1,13 +1,16 @@
 import json
+import ollama
 from ollama import chat
 from ollama import ChatResponse
 
-with open("ollama/asclepius_notes.json") as fp:
+with open("asclepius_notes.json") as fp:
     notes = json.load(fp)
 
-for note in notes:
+print(ollama.list())
+
+for note in notes[:3]:
     prompt = f'Here is a clinical note, please analyze it for the presence of medical implants. Answer as a JSON object with the format {{"has_implant": true/false, "implant": ""/"pacemake"}}: {note}'
-    response: ChatResponse = chat(model='llama3.2', messages=[
+    response: ChatResponse = chat(model='phi4:latest', messages=[
     {
         'role': 'user',
         'content': 'Why is the sky blue?',
