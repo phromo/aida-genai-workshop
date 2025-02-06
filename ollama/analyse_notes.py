df98a7578583b30a2a17d80153522adc36aff995
Erik Ylipää <erik.ylipaa@gmail.com>
2025-02-06 08:50:28 +0100

    Minor fix

--- ollama/analyse_notes.py
+++ ollama/analyse_notes.py
@@ -6,16 +6,17 @@ from ollama import ChatResponse
 with open("asclepius_notes.json") as fp:
     notes = json.load(fp)
 
-print(ollama.list())
+#print(ollama.list())
 
-for note in notes[:3]:
+for note in notes[:5]:
     prompt = f'Here is a clinical note, please analyze it for the presence of medical implants. Answer as a JSON object with the format {{"has_implant": true/false, "implant": ""/"pacemake"}}: {note}'
     response: ChatResponse = chat(model='phi4:latest', messages=[
     {
         'role': 'user',
-        'content': 'Why is the sky blue?',
+        'content': prompt,
     },
     ])
     print(response['message']['content'])
     # or access fields directly from the response object
     print(response.message.content)
+
